9b4cf61b176d4d005e657e7a93f0561e71b995e3
Hugo Acosta <hugoxacme@gmail.com>
2024-08-07 21:04:13 -0600

    clase variables

---
+++ codigo.js
@@ -0,0 +1,22 @@
+/*console.log("hugo")
+
+var metros = 1250;
+
+var kilometros = metros/1000;
+console.log('en kilometros es '+kilometros);
+
+var metros = prompt('cuantos kilometros son: ')
+var kilometros = metros/1000;
+console.log('en kilometros es '+ kilometros);
+
+var peso = prompt('Cuanto pesas?');
+var altura = prompt('Cuanto mides?')
+
+var imc = peso / (altura*altura);
+
+console.log("tu imc es " + imc);*/
+
+var nombre = prompt('infresa tu nombre');
+var apelliP = prompt('Ingresa apellido paterno');
+var apelliM = prompt('Ingresa apellido materno');
+var nombreCompleto = console.log(nombre + " "+ apelliP + " " + apelliM );
